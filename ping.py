2068a3fb9b610bc4fb94dc1f114ef1f567e2fd86
gung <71138589+agungsoboru@users.noreply.github.com>
2023-01-22 22:50:24 +0800

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,25 +1,17 @@
 import os
-import requests
-import time
-ip_address = "192.168.1.43"
-previous_status = ""
+
+status = "down"
 
 while True:
-    response = os.system("ping -c 1 " + ip_address)
-    time.sleep(20)
-
-
+    response = os.system("ping -c 1 192.168.1.43")
     if response == 0:
-        previous_status = "down"
-        #if previous_status == "up":
-            print("Server sudah down, melakukan request...")
-            try:
-                r = requests.get("https://api.telegram.org/bot786786482742387462/sendMessage?chat_id=123123123123?disable_notification=true&text=Server 192.168.1.43 down")
-                print(r.text)
-            except requests.exceptions.RequestException as e:
-                print("Request gagal:", e)
-            #previous_status = "down"
+        if status == "down":
+            print("Up")
+            r = requests.get("https://api.telegram.org/bot786786482742387462/sendMessage?chat_id=123123123123?disable_notification=true&text=Server 192.168.1.43 down")
+            status = "up"
     else:
-        previous_status = "up"
-        #r = requests.get("https://api.telegram.org/bot5454378564735643765478563874/sendMessage?chat_id=4654646456456456?disable_notification=true&text=Server sudah up")
-        print("Server sudah up")
+        if status == "up":
+            print("Down")
+            r = requests.get("https://api.telegram.org/bot786786482742387462/sendMessage?chat_id=123123123123?disable_notification=true&text=Server 192.168.1.43 down")
+            status = "down"
+    time.sleep(1)
